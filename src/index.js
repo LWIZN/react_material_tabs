13a5c55203b9cddb9dfb5861be9729308156eb9d
eddy0117 <tony23068527@gmail.com>
2022-08-14 14:34:40 +0800

    base

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import LabTabs from "./components/LabTabs";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
